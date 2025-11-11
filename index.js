01eeb2ad4bd27eb9fcad7bb56a834373ab369c0f
Saurav s Mehta <sauravmehta631@gmail.com>
2025-11-11 16:52:08 +0530

    ejs

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+let Port = 3000;
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname, "/views"));
+
+app.get("/",(req,res)=>{
+    res.render("home");
+})
+
+app.get("/insta/:username",(req,res)=>{
+    let {username} = req.params;
+    res.render("insta",{username});
+})
+
+// app.use((req, res)=>{
+//     res.send("all routes");
+// })
+
+app.listen(Port,()=>{
+    console.log(`Listening to Port : ${Port}`);
+})
