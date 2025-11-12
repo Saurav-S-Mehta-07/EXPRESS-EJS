5ffc72f2f0c2122090abd14aa1055c6e1e9a8e17
Saurav s Mehta <sauravmehta631@gmail.com>
2025-11-12 18:34:13 +0530

    quora posts done

--- Quora/index.js
+++ Quora/index.js
@@ -1,41 +1,74 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const uuid = require("uuid").v1;
+const methodOverride = require("method-override");
 
 let Port = 3000;
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname,"/views"));
-app.use(express.static(path.join(__dirname,"/public")));
+app.set("views", path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(methodOverride("_method"));
 
 let posts = [];
 
-app.get("/",(req,res)=>{
+app.get("/posts",(req,res)=>{
     res.render("index",{posts});
 });
 
-
-
-app.post("/",(req,res)=>{
-    let details = req.body;
-    posts.push(details);
-    console.log(posts);
-    res.redirect("/");
+app.patch("/posts/editpost/:id",(req,res)=>{
+    let {id} = req.params;
+    let {username, description} = req.body;
+    let post = posts.find((a)=>a.id === id);
+    if(!id || !post){
+        res.render("error");
+        return;
+    }
+    post.username = username;
+    post.description = description;
+    post.id = id;
+    res.redirect(`/posts/show/${id}`);
 })
 
-app.get("/addpost",(req,res)=>{
+app.post("/posts",(req,res)=>{
+    let details = req.body;
+    details.id = uuid();
+    posts.push(details);
+    res.redirect("/posts");
+})
+
+app.delete("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    posts = posts.filter((a)=>a.id != id);
+    res.redirect("/posts");
+})
+
+app.get("/posts/addpost",(req,res)=>{
     res.render("addpost");
 })
 
-app.get("/editpost",(req,res)=>{
-    res.send("This is the edit post page");
+app.get("/posts/editpost/:id",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((a)=>a.id === id);
+    res.render("editpost",{post});
 })
 
-app.post("/deletepost",(req,res)=>{
-    res.send("This is the delete post page");
+app.get("/posts/show/:id",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((a)=>a.id === id);
+    if(!id || !post){
+        res.render("error");
+        return;
+    }
+    res.render("show",{post});
 });
+
+app.use((req,res)=>{
+    res.render("error");
+})
 
 
 app.listen(Port,()=>{
